91e058c8ee88cf2aff46e0de2662ff85dc23931d
PaulSoia <p-shadow@ukr.net>
2025-04-21 17:09:54 +0300

    connect db

--- src/main/kotlin/com/spasinnya/Databases.kt
+++ src/main/kotlin/com/spasinnya/Databases.kt
@@ -15,10 +15,15 @@ object DatabaseFactory {
     }
 
     fun init() {
-        val url = "jdbc:postgresql://localhost:5432/mentoring_db"
+        /*val url = "jdbc:postgresql://localhost:5432/mentoring_db"
         val driver = "org.postgresql.Driver"
         val user = "postgres"
-        val password = "root"
+        val password = "root"*/
+
+        val url = "postgresql://mentoring_db_user:zjdcYhGc3wmxlPpuxV6N2Y7h5HhxLrRx@dpg-d034v1idbo4c73c9phn0-a/mentoring_db"
+        val driver = "org.postgresql.Driver"
+        val user = "mentoring_db_user"
+        val password = "zjdcYhGc3wmxlPpuxV6N2Y7h5HhxLrRx"
 
         Database.connect(
             url = url,
